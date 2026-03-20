051867af10f1172e911c3b99cf5ad0675993fec7
codeEnjoyer7 <nicolas.nl.londono@gmail.com>
2026-03-20 11:04:14 -0400

    refactored the server side pieces and how rooms are stored. Begun coding so that the server validates the clients requests

--- index.js
+++ index.js
@@ -17,32 +17,6 @@ app.get('/', (req, res) => {
 
 const roomsData = new Map();
 
-io.on('connection', (socket) => {
-
-  socket.on("CreateRoom", (arg, callback) => {
-    const roomId = nanoid(6);
-    socket.join(roomId);
-
-    roomsData.set(roomId, {
-      creatorId: arg
-    });
-
-    socket.emit('returnRoomId', roomId);
-
-  });
-
-  socket.on("JoinRoom", (arg, callback) => {
-    if(io.sockets.adapter.rooms.has(arg.roomId)){
-
-      let data = roomsData.get(arg.roomId);
-      roomsData.set(arg.roomId, {
-        creatorId: data.creatorId,
-        joinerId: arg.joinerId
-      });
-
-      data = roomsData.get(arg.roomId);
-      //server-side game logic goes below here?
-
       class King{
         constructor(team, tile){
             this.team=team;
@@ -104,15 +78,59 @@ io.on('connection', (socket) => {
             this.team=team;
             this.tile=tile;
             this.symbol="p";
+            this.hasMoved=false;
         }
 
         getTeam(){
           return this.team;
         }
+
+        getMoves(board){
+          let availableMoves=[]
+          console.log("tile: ",this.tile);
+          if(board[this.tile-8]==null){
+            availableMoves.push(this.tile-8)
+            if(board[this.tile-16]==null && this.hasMoved==false){
+              availableMoves.push(this.tile-16)
+            }
+          }
+          console.log("available moves: ", availableMoves);
+          return availableMoves;
+        }
     }
 
-      let doesCreatorGoFirst = Math.floor(Math.random() * 2);
-      let board=[new Rook(2,0,0), new Knight(2,0,1), new Bishop(2,0,2), new Queen(2,0,3), new King(2,0,4), new Bishop(2,0,5), new Knight(2,0,6), new Rook(2,0,7), new Pawn(2,1,0), new Pawn(2,1,1), new Pawn(2,1,2), new Pawn(2,1,3), new Pawn(2,1,4),new Pawn(2,1,5), new Pawn(2,1,6), new Pawn(2,1,7), null,null,null,null,null,null,null,null, null,null,null,null,null,null,null,null, null,null,null,null,null,null,null,null, null,null,null,null,null,null,null,null, new Pawn(1,6,0), new Pawn(1,6,1), new Pawn(1,6,2), new Pawn(1,6,3), new Pawn(1,6,4),new Pawn(1,6,5), new Pawn(1,6,6), new Pawn(1,6,7), new Rook(1,7,0), new Knight(1,7,1), new Bishop(1,7,2), new Queen(1,7,3), new King(1,7,4), new Bishop(1,7,5), new Knight(1,7,6), new Rook(1,7,7)];
+io.on('connection', (socket) => {
+
+  socket.on("CreateRoom", (arg, callback) => {
+    const roomId = nanoid(6);
+    socket.join(roomId);
+    let isCreatorFirst = Math.floor(Math.random() * 2);
+    roomsData.set(roomId, {
+      creatorId: arg,
+      isCreatorFirst : isCreatorFirst,
+      board : [new Rook(2,0), new Knight(2,1), new Bishop(2,2), new Queen(2,3), new King(2,4), new Bishop(2,5), new Knight(2,6), new Rook(2,7), new Pawn(2,8), new Pawn(2,9), new Pawn(2,10), new Pawn(2,11), new Pawn(2,12),new Pawn(2,13), new Pawn(2,14), new Pawn(2,15), null,null,null,null,null,null,null,null, null,null,null,null,null,null,null,null, null,null,null,null,null,null,null,null, null,null,null,null,null,null,null,null, new Pawn(1,48), new Pawn(1,49), new Pawn(1,50), new Pawn(1,51), new Pawn(1,52),new Pawn(1,53), new Pawn(1,54), new Pawn(1,55), new Rook(1,56), new Knight(1,57), new Bishop(1,58), new Queen(1,59), new King(1,60), new Bishop(1,61), new Knight(1,62), new Rook(1,63)]
+    });
+
+    socket.emit('returnRoomId', roomId);
+
+  });
+
+  socket.on("JoinRoom", (arg, callback) => {
+    if(io.sockets.adapter.rooms.has(arg.roomId)){
+
+      let data = roomsData.get(arg.roomId);
+      roomsData.set(arg.roomId, {
+        creatorId: data.creatorId,
+        joinerId: arg.joinerId,
+        isCreatorFirst: data.isCreatorFirst,
+        currentTurn: 1,
+        board: data.board
+      });
+
+      data = roomsData.get(arg.roomId);
+      //server-side game logic goes below here?
+
+      let board=data.board;
 
       let stringBoard ="";
       let emptySquares=0;
@@ -145,12 +163,12 @@ io.on('connection', (socket) => {
       }
       stringBoard=stringBoard.slice(0,-1);
       //console.log(arg);
-      
-      if(doesCreatorGoFirst==1){
+      console.log(data.isCreatorFirst)
+      if(data.isCreatorFirst==1){
         io.to(data.creatorId).emit("ReturnBoard", {room: arg.roomId,team: 1, isMyTurn: true, stringBoard: stringBoard});
         io.to(data.joinerId).emit("ReturnBoard", {room: arg.roomId,team: 2, isMyTurn: false, stringBoard: stringBoard});
       }
-      else if(doesCreatorGoFirst==0){
+      else if(data.isCreatorFirst==0){
         io.to(data.creatorId).emit("ReturnBoard", {room: arg.roomId, team: 2, isMyTurn: false, stringBoard: stringBoard});
         io.to(data.joinerId).emit("ReturnBoard", {room: arg.roomId, team: 1, isMyTurn: true, stringBoard: stringBoard});
       }
@@ -164,6 +182,18 @@ io.on('connection', (socket) => {
 
   socket.on("requestMove", (arg, callback) =>{
     console.log(arg);
+    let data = roomsData.get(arg.roomId);
+    console.log(data.board[arg.fromTile]);
+    console.log(data.board[arg.fromTile].getMoves(data.board).includes(arg.toTile));
+
+    if(data.board[arg.fromTile].getMoves(data.board).includes(arg.toTile)){ //if it is a valid move
+      if(socket.id == data.creatorId && (data.isCreatorFirst && data.currentTurn%2==1) || (!data.isCreatorFirst && data.currentTurn%2==0)){//if its whites turn and they sent a request
+        console.log("CREATOR REQUEST VALID")
+      }
+      else if(socket.id == data.joinerId && (data.isCreatorFirst && data.currentTurn%2==0) ||(!data.isCreatorFirst && data.currentTurn%2==1) ){//if its blacks turn and they sent a request
+        console.log("JOINER REQUEST VALID")
+      }
+    }
   });
 
 });
